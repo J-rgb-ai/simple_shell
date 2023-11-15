656b0ee144157a3eed8b1b21b67fcab0b77940df
J-rgb-ai <opiyojoyshaney.com>
2023-11-15 09:23:10 -0800

    task 1

---
+++ betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: sum
+ */
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	int sum;
+
+	sum = a + b;
+	printf("%d\n", sum);
+	return (sum);
+}
